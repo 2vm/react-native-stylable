1f1100a7ce126b2faa563174e24d07ac8693685a
korgara <korgara93@gmail.com>
2018-10-16 14:53:58 +0300

    fixed isPureComponent error

--- dist/isPureComponent.js
+++ dist/isPureComponent.js
@@ -1,2 +1,1 @@
-Object.defineProperty(exports,"__esModule",{value:true});function isPureComponent(comp){if(typeof comp==='string'){return false;}var p=comp.prototype;
-return p && (p.render || p.isPureReactComponent || !p.setState);}exports.default=isPureComponent;
+Object.defineProperty(exports, "__esModule", { value: true }); function isPureComponent(comp) { if (typeof comp === 'string' || comp.$$typeof === Symbol.for('react.forward_ref')) { return false; } var p = comp.prototype; return p && (p.render || p.isPureReactComponent || !p.setState); } exports.default = isPureComponent;
