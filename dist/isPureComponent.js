b1c8532721d995fa36574e72fc2c56b000d92b62
Vladimir Matsola <vladimir.matsola@litslink.com>
2018-10-16 14:46:49 +0300

    Update isPureComponent.js

--- dist/isPureComponent.js
+++ dist/isPureComponent.js
@@ -1,1 +1,2 @@
-Object.defineProperty(exports,"__esModule",{value:true});function isPureComponent(comp){if(typeof comp==='string'){return false;}var p=comp.prototype;return p.isPureReactComponent||!p.setState;}exports.default=isPureComponent;
+Object.defineProperty(exports,"__esModule",{value:true});function isPureComponent(comp){if(typeof comp==='string'){return false;}var p=comp.prototype;
+return p && (p.render || p.isPureReactComponent || !p.setState);}exports.default=isPureComponent;
