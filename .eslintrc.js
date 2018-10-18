03c7eb977be8eff19acb6ff470d9eed6a1dce218
korgara <korgara93@gmail.com>
2018-10-18 10:45:38 +0300

    fixed isPureComponent error

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,6 @@ module.exports = {
   plugins: [ 'import' ],
   parser: 'babel-eslint',
   rules: {
-    // For readability (TODO: sort imports in same group)
     'import/order': ['error',{'newlines-between': 'always'}],
   }
 }
